4467ab2013e57e038ee32e36d25b627252ba8e8e
Santoshb22 <sb289848@gmail.com>
2024-12-14 08:54:21 +0530

    error handling in artists page

--- app/artists/page.js
+++ app/artists/page.js
@@ -2,21 +2,28 @@
 import ArtistCard from "@/components/ArtistCard";
 
 const Artists = async () => {
-  const artistAPI = process.env.ARTIST_API_URL;
-  const response = await fetch(artistAPI);
-  const artistsData = await response.json();
+  const artistAPI = process.env.NEXT_PUBLIC_ARTIST_API_URL;
 
-  return (
-    <div className="max-w-[100%] w-[95%] mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-    {
-      artistsData.length > 0? (
-        artistsData.map(artistData => <ArtistCard key={artistData.id} artistData = {artistData}/>)
-      ) : (
-        <h2>No Artist Available</h2>
-      )
-    }
-    </div>
-  )
+  try {
+    const response = await fetch(artistAPI);
+    const artistsData = await response.json();
+  
+    return (
+      <div className="max-w-[100%] w-[95%] mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+      {
+        artistsData.length > 0? (
+          artistsData.map(artistData => <ArtistCard key={artistData.id} artistData = {artistData}/>)
+        ) : (
+          <h2>No Artist Available</h2>
+        )
+      }
+      </div>
+    )
+  } catch (error) {
+    console.error(error.message);
+    return <h2>Error Loading Artists</h2>
+  }
+
 }
 
 export default Artists
